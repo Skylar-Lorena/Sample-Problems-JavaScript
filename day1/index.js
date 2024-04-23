2ad41e86ade35fa79ead028d35bc8f89127637fe
Skylar-Lorena <lorayne911@gmail.com>
2024-04-23 10:04:51 +0300

    define array of objects and selct DOM elements to display them

---
+++ day1/index.js
@@ -0,0 +1,13 @@
+// create an array of quotes objects- quotes and author
+const quotes = [
+    {quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde"},
+    {quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde"},
+    {quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde"},
+    {quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde"},
+    {quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde"}
+];
+
+// select the DOM elements for displaying quote & author
+const quoteText = document.querySelector('.quote');
+const authorText = document.querySelector('.author');
+const generateBtn = document.getElementById('#generate-btn');
