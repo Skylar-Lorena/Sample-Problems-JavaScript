18277632a14018f1c8d21e3c6c0d7012bbdb257c
Skylar-Lorena <lorayne911@gmail.com>
2024-04-23 10:35:39 +0300

    check: retrieve author and quote text

--- day1/index.js
+++ day1/index.js
@@ -6,7 +6,6 @@ const quotes = [
     {quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde"},
     {quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde"}
 ];
-
 // select the DOM elements for displaying quote & author
 const quoteText = document.querySelector('.quote');
 const authorText = document.querySelector('.author');
@@ -15,5 +14,9 @@ const generateBtn = document.getElementById('#generate-btn');
 // add click event listener
 generateBtn.addEventListener('click', ()=>{
     //generate random index within quotes array
-    const randomIndex = Math.floor(Math.random()* quotes.length);
-})
+    console.log("fine");
+    const randomIndex = Math.floor(Math.random()* quotes.length); 
+    const quote = quotes[randomIndex]; 
+    quoteText.textContent =`${quote.quote}`;
+    authorText.textContent = `${quote.author}`;
+});
